518c3a25c6d2805411617a02fc39eb4a7168c285
ksks2211 <rival15@naver.com>
2021-12-21 14:07:58 +0900

    buffer examples

--- src/nio/buffer/BufferExample.java
+++ src/nio/buffer/BufferExample.java
@@ -1,2 +1,21 @@
-package nio.buffer;public class BufferExample {
+package nio.buffer;
+
+import java.nio.ByteBuffer;
+
+public class BufferExample {
+    public static void main(String[] args) {
+        try {
+            ByteBuffer directBuffer = ByteBuffer.allocateDirect(2000 * 1024 * 1024);
+            System.out.println("Direct Buffer Created");
+        }catch(Exception e){
+            System.out.println("Direct Buffer Creation Failed");
+        }
+
+        try{
+            ByteBuffer nonDirectBuffer = ByteBuffer.allocate(2000*1024*1024);
+            System.out.println("Non-Direct Buffer Created");
+        }catch(Exception e){
+            System.out.println("Non-Direct Buffer Creation Failed");
+        }
+    }
 }
